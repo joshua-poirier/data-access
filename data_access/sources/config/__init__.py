41471f9a6dd8000756f2b03e9c3d1f62c5cb4ba3
joshuaadampoirier <josh.poirier@gmail.com>
2024-09-22 18:24:58 -0400

    feat: add an AWSSession object populated using env variables

--- data_access/sources/config/__init__.py
+++ data_access/sources/config/__init__.py
@@ -1,3 +1,4 @@
+from .aws_session import AWSSession
 from .google_drive_service_account_info import GoogleDriveServiceAccountInfo
 
-__all__ = ["GoogleDriveServiceAccountInfo"]
+__all__ = ["AWSSession", "GoogleDriveServiceAccountInfo"]
